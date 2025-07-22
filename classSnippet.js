20c7a4e81a547b42438ee69c3ab6c192fe34624a
assfrdz <assia.ferradj.sio@gmail.com>
2025-07-22 22:55:07 +0200

    insertion

---
+++ classSnippet.js
@@ -0,0 +1,14 @@
+export class Snippet{
+    title;
+    langage;
+    code;
+    explanation;
+
+    constructor(title,langage,code,explanation){
+        this.title = title;
+        this.langage = langage;
+        this.code = code;
+        this.explanation = explanation;
+    }
+
+}
---
+++ script.js
@@ -0,0 +1,32 @@
+var buttons = document.querySelectorAll(".cta-1-dark");
+
+buttons.forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        console.log(btn.classList);
+        activeBtn(btn);
+
+        if(btn.textContent.includes("Copier") || btn.textContent.includes("Copié")){
+            copierToClipboard(btn);
+        }
+    })
+
+})
+
+function activeBtn(btn){
+    btn.classList.toggle("active");
+}
+
+function copierToClipboard(btn){
+   
+    var text = btn.parentElement.querySelector("code").textContent;
+
+    navigator.clipboard.writeText(text).then(function() {
+        btn.classList.add("active");
+        btn.innerHTML = `Copié  <i class='fa-solid fa-check'></i>`;
+        console.log("copié");
+
+    }, function(err) {
+        console.error('Erreur lors de la copie dans le presse-papiers: ', err);
+    });
+
+}
